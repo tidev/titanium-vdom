36be4b7586752fcc9b94b92f0101b9af16993041
Jan Vennemann <jan.vennemann@gmx.net>
2018-06-21 23:37:13 +0200

    test: use new constructor signature

--- test/unit/helpers.ts
+++ test/unit/helpers.ts
@@ -1,7 +1,7 @@
 import { TitaniumElement } from 'vdom/elements/TitaniumElement';
 
 export function createElement(tagName: string, options?: {[k: string]: any}) {
-    const element = new TitaniumElement(tagName, o => (Ti.UI as any)[`create${tagName}`](o));
+    const element = new TitaniumElement(tagName, o => (Ti.UI as any)[`create${tagName}`](o), { typeName: `Ti.UI.${tagName}` });
     if (options) {
         Object.keys(options).forEach(optionKey => {
             element.setAttribute(optionKey, options[optionKey]);
