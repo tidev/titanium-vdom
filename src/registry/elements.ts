29678f831234307a3718fe40a90048cf96901e6a
Jan Vennemann <jan.vennemann@gmx.net>
2018-06-30 22:14:35 +0200

    fix: load all elements for AoT compilers

--- src/registry/elements.ts
+++ src/registry/elements.ts
@@ -1,6 +1,9 @@
+import { registerTitaniumElements as registerAndroidElements } from './elements.android';
+import { registerTitaniumElements as registerIosElements } from './elements.ios';
 import { TitaniumElementRegistry } from './TitaniumElementRegistry';
 
+/* istanbul ignore next: universal registration for AoT template compilation, cannot be tested on a device */
 export function registerTitaniumElements(registry: TitaniumElementRegistry): void {
-    // Empty dummy function to make the titanium-vue-template-compiler happy. It's built
-    // using rollup which is not able to load platform specific files.
+    registerAndroidElements(registry);
+    registerIosElements(registry);
 }
