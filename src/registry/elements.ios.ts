798ae655ea2eedbdaefd17facf48da0a7be527e3
Jan Vennemann <jan.vennemann@gmx.net>
2018-09-18 00:48:58 +0200

    feat: add platform elements

--- src/registry/elements.ios.ts
+++ src/registry/elements.ios.ts
@@ -5,8 +5,11 @@ export function registerTitaniumElements(registry: TitaniumElementRegistry) {
     registerCommonTitaniumElements(registry);
 
     registry.registerElement('BlurView', () => Ti.UI.iOS.createBlurView, { typeName: 'Ti.UI.iOS.BlurView' });
+    registry.registerElement('ButtonBar', () => Ti.UI.createButtonBar, { typeName: 'Ti.UI.iOS.ButtonBar' });
     registry.registerElement('DashboardView', () => Ti.UI.createDashboardView, { typeName: 'Ti.UI.DashboardView' });
     registry.registerElement('DashboardItem', () => Ti.UI.createDashboardItem, { typeName: 'Ti.UI.DashboardItem' });
+    registry.registerElement('LivePhotoView', () => Ti.UI.iOS.createLivePhotoView, { typeName: 'Ti.UI.iOS.LivePhotoView' });
     registry.registerElement('NavigationWindow', () => Ti.UI.iOS.createNavigationWindow, { typeName: 'Ti.UI.iOS.NavigationWindow', detached: true });
+    registry.registerElement('Stepper', () => Ti.UI.iOS.createStepper, { typeName: 'Ti.UI.iOS.Stepper' });
     registry.registerElement('TabbedBar', () => Ti.UI.iOS.createTabbedBar, { typeName: 'Ti.UI.iOS.TabbedBar' });
 }
